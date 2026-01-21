fd368efdce1356d1a26c0e28f620eb50ec100116
CorbeauBaptiste <corbeaubaptiste2005@gmail.com>
2026-01-21 10:59:22 +0100

    étape 2

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Book> books = new List<Book>
+        {
+            new Book { Name = "Le Seigneur des Anneaux", Type = "Fantaisie" },
+            new Book { Name = "Harry Potter", Type = "Fantaisie" },
+            new Book { Name = "Indiana Jones", Type = "Aventure" }
+        };
+
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.Name);
+        }
+    }
+}
+
+class Book
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+}
