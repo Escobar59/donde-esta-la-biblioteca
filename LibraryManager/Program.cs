aa4d71d1d6f4b53480e46675912f5250b5282a7d
CorbeauBaptiste <corbeaubaptiste2005@gmail.com>
2026-01-21 14:18:44 +0100

    étape 4.1

---
+++ LibraryManager/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using BusinessObjects.Entity;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var repo = new BookRepository();
+        var books = repo.GetAll();
+
+        var adventureBooks = books.Where(book => book.Type == TypeBook.Aventure);
+        foreach (var book in adventureBooks)
+        {
+            Console.WriteLine(book.Name);
+        }
+    }
+}
