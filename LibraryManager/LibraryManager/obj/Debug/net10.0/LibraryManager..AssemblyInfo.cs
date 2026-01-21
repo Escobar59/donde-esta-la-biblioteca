840dcaf85b49a4f324d139c2f025d39f5bbfd6d3
Clement Cotar <clementcotar59@gmail.com>
2026-01-21 10:12:59 +0100

    Suppression .vs

--- LibraryManager/LibraryManager/obj/Debug/net10.0/LibraryManager..AssemblyInfo.cs
+++ LibraryManager/LibraryManager/obj/Debug/net10.0/LibraryManager..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManager.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1c2971487147d0657cdeda3cb2f01932c38bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManager.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManager.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
